3c154f6cee6f7e0fc9827d120938b480f09d3f5f
raymeeeee <daniapoluektov@gmail.com>
2024-10-14 15:35:02 +0100

    Create Employee declaration

--- lesson29cw/Employee.cpp
+++ lesson29cw/Employee.cpp
@@ -1,1 +1,44 @@
 #include "Employee.h"
+
+Employee::Employee()
+{
+    name = "undefined";
+    salary = 7000; //minimal
+}
+
+Employee::Employee(string name, float salary)
+{
+    this->name = name;
+    this->salary = salary;
+}
+
+string Employee::getName() const
+{
+    return name;
+}
+
+float Employee::getSalary() const
+{
+    return salary;
+}
+
+void Employee::setName(string name)
+{
+    this->name = name;
+}
+
+void Employee::setSalary(float salary)
+{
+    this->salary = salary;
+}
+
+void Employee::showInfo() const
+{
+    cout << "Name: " << name << endl;
+    cout << "Salary: " << salary << endl;
+}
+
+float Employee::calcSalary() const
+{
+    return salary*0.83; 
+}
