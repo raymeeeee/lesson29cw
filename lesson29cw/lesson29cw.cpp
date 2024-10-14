0907d6b2122c01fe9224a172412637d3bb0fb7be
raymeeeee <daniapoluektov@gmail.com>
2024-10-14 17:19:00 +0100

    create sort and TeacherSalary+100

--- lesson29cw/lesson29cw.cpp
+++ lesson29cw/lesson29cw.cpp
@@ -3,7 +3,7 @@
 
 #include <vector>
 #include <typeinfo>
-
+#include <algorithm>
 int main()
 {
     Employee a("Brazhnik Andriy", 15'000);
@@ -21,7 +21,7 @@ int main()
     vector<Employee*> itStep = {&a, &b, &c};
     itStep.push_back(new Manager("Smagin Oleksandr", 32'000, 15, 96'000));
     itStep.push_back(new Manager("Chelishev Ivan", 22'000, 50, 25'900));
-    itStep.push_back(new Teacher("Hiyalov Ruslan", 29'000, 35, 52'200));
+    itStep.push_back(new Teacher("Hiyalov Ruslan", 29'000, 350, 20));
     itStep.push_back(new Employee("Poluektov Danylo", 55'000));
 
     for (int i = 0; i < itStep.size(); i++) //7
@@ -31,17 +31,41 @@ int main()
     }
     cout << "======================================" << endl;
 
-    cout << "Managers:\n" << endl;
-    for (int i = 0; i < itStep.size(); i++) //7
-    {
-        if (typeid(*itStep[i]) == typeid(Manager)) {
-            cout << "Name: " << itStep[i]->getName() << endl;
-        }     
+    //cout << "Managers:\n" << endl;
+    //for (int i = 0; i < itStep.size(); i++) //7
+    //{
+    //    if (typeid(*itStep[i]) == typeid(Manager)) {
+    //        cout << "Name: " << itStep[i]->getName() << endl;
+    //    }     
+    //}
+    //cout << "======================================" << endl;
+
+    //cout << "Teachers:\n" << endl;
+    //for (int i = 0; i < itStep.size(); i++) //7
+    //{
+    //    if (typeid(*itStep[i]) == typeid(Teacher)) {
+    //        cout << "Name: " << itStep[i]->getName() << endl;
+    //        //Teacher* p = (Teacher*)itStep[i];
+    //        Teacher* p = dynamic_cast<Teacher*>(itStep[i]); //dynamic_cast employee, manager to teacher = NULLPTR
+    //        cout << "Old cost: " << p->getCost() << endl;
+    //        p->setCost(p->getCost() + 100);
+    //        cout << "New cost: " << p->getCost() << endl;
+    //        cout << endl;
+    //    }
+    //}
+    //cout << "======================================" << endl;
+
+    sort(itStep.begin(), itStep.end()); //sort by address
+    sort(itStep.begin(), itStep.end(), [](Employee* a, Employee* b) {return a->calcSalary() > b->calcSalary(); });
+
+    for (auto item : itStep) {
+        item->showInfo();
+        cout << endl;
     }
 
-    for (int i = 3; i < itStep.size(); i++) 
+    /*for (int i = 3; i < itStep.size(); i++) 
     {
         delete itStep[i];
-    }
+    }*/
 }
 
