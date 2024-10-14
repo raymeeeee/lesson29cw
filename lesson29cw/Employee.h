d71136b7951f9ce913db5dfa045728e5877c6c11
raymeeeee <daniapoluektov@gmail.com>
2024-10-14 15:31:45 +0100

    Create Employee definition

--- lesson29cw/Employee.h
+++ lesson29cw/Employee.h
@@ -1,9 +1,25 @@
 #pragma once
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Employee
 {
+protected:
+	string name;
+	float salary;
+public:
+	Employee();
+	Employee(string name, float salary);
 
+	string getName()const;
+	float getSalary()const;
+
+	void setName(string name);
+	void setSalary(float salary);
+
+	void showInfo()const;
+
+	float calcSalary()const;
 };
 
